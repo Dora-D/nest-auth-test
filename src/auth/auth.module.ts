596d307fbce05dfab33d00d1e259063f80575b64
Danil Zaychenko <danil.zaychenko@headworks.com.ua>
2023-08-03 23:24:35 +0300

    added at rt strategies

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,15 @@
+import { AuthController } from './auth.controller';
 import { User } from '@app/user/entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { AuthService } from '@app/auth/auth.service';
+import { AtStrategy } from '@app/auth/strategies/at.strategy';
+import { RtStrategy } from '@app/auth/strategies/rt.strategy';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  providers: [AuthService],
+  imports: [TypeOrmModule.forFeature([User]), JwtModule.register({})],
+  controllers: [AuthController],
+  providers: [AuthService, AtStrategy, RtStrategy],
 })
 export class AuthModule {}
