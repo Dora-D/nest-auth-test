0edf505caa7261bff783a9365e9397b228fa2f44
Danil Zaychenko <danil.zaychenko@headworks.com.ua>
2023-08-03 23:23:53 +0300

    added http codes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,27 +1,32 @@
-import { Controller, Post } from '@nestjs/common';
+import { AuthDto } from '@app/auth/dto/auth.dto';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from '@app/auth/auth.service';
+import { Tokens } from '@app/auth/types/tokens.type';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
-
-  @Post('/local/signup')
-  signupLocal() {
-    return this.authService.signupLocal();
+  constructor(private readonly authService: AuthService) {}
+  @HttpCode(HttpStatus.CREATED)
+  @Post('local/signup')
+  signupLocal(@Body() dto: AuthDto): Promise<Tokens> {
+    return this.authService.signupLocal(dto);
   }
 
-  @Post('/local/signin')
-  signinLocal() {
-    return this.authService.signinLocal();
+  @HttpCode(HttpStatus.OK)
+  @Post('local/signin')
+  signinLocal(@Body() dto: AuthDto): Promise<Tokens> {
+    return this.authService.signinLocal(dto);
   }
 
-  @Post('/logout')
-  logout() {
-    return this.authService.logout();
-  }
+  // @HttpCode(HttpStatus.OK)
+  // @Post('logout')
+  // logout(userId: number): Promise<boolean> {
+  //   return this.authService.logout(userId);
+  // }
 
-  @Post('/refresh')
-  refresh() {
-    return this.authService.refresh();
-  }
+  // @HttpCode(HttpStatus.OK)
+  // @Post('refresh')
+  // refreshTokens(): Promise<Tokens> {
+  //   return this.authService.refreshTokens();
+  // }
 }
